b7e877e1a3d80492d5589ce02a05acfb67609c5b
Azis4215 <krnshinigami@gmail.com>
2026-01-29 10:26:00 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://bakti45.or.id',
+      url: 'https://www.bakti45sejahtera.my.id/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://bakti45.or.id/profil',
+      url: 'https://www.bakti45sejahtera.my.id/profil',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
     },
     {
-      url: 'https://bakti45.or.id/kegiatan',
+      url: 'https://www.bakti45sejahtera.my.id/kegiatan',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: 'https://bakti45.or.id/kontak',
+      url: 'https://www.bakti45sejahtera.my.id/kontak',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
